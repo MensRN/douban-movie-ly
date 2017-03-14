1de2f2727ab151e595dcd5d4d2dfb2feea501365
Luo Yang <Lyyx_0@163.com>
2017-03-14 15:09:49 +0800

    update star

--- MainApp/view/star.js
+++ MainApp/view/star.js
@@ -6,18 +6,27 @@ export default class Star extends Component {
 
     renderList() {
         var starList = new Array(5);
+        console.log("-------------------------------------------------");
+        var rating = Math.floor(this.props.rating);
         for (var i = 1; i <= 5; i++) {
-            ratingSplit = this.props.rating - 2 * i;
-            starList[i-1] = ratingSplit;
+            if (rating >= 2 * i) {
+                starList[i] = 2;
+            } else if ((rating < 2 * i)&&(rating > 2 * (i-1))) {
+                starList[i] = 1;
+            } else {
+                starList[i] = 0;
+            }
+            console.log("index:" + i + " item:", rating + " i*2:" + i * 2 + " starList:" + starList[i]);
         }
-        return starList.map((item,i) => this.renderItem(item,i));
+
+        return starList.map((item, i) => this.renderItem(item, i));
     }
 
     renderItem(item, i) {
-        if (item >= 0) {
+        if (item == 2) {
             return <Image key={i} source={require('../resources/star.png')} />;
         }
-        else if (item > -2) {
+        else if (item == 1) {
             return <Image key={i} source={require('../resources/starHalf.png')} />;
         }
         else {
@@ -28,7 +37,7 @@ export default class Star extends Component {
     render() {
         return (
             <View style={{ flexDirection: 'row', }}>
-                <Text style={{paddingRight:3}}>
+                <Text style={{ paddingRight: 3 }}>
                     {this.props.rating}
                 </Text>
                 <View style={{ flexDirection: 'row', }}>
